d4c2186dbb6eed32f2a86d5c658d93ad045ec9cd
praneeth <praneethkairoju8694@gmail.com>
2020-11-08 16:37:34 +0530

    settings in decouple

--- sdrkproject/settings.py
+++ sdrkproject/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 from decouple import config
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
