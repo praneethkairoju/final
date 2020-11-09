85429055919721bc9ff230093a9e961b0a0f0671
praneeth <praneethkairoju8694@gmail.com>
2020-11-09 16:53:11 +0530

    commit settings

--- sdrkproject/settings.py
+++ sdrkproject/settings.py
@@ -29,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=False)
+DEBUG = config('DEBUG')
 
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
